fe44c512adcc56e9d4ea11b226b33509fa69fede
loyal_goose <polina.alina.2015@mail.ru>
2023-05-16 15:14:39 +0700

    feat: добавить роут

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,22 @@
+import { CoursePage } from "./pages/CoursePage/CoursePage";
 import { Courses } from "./pages/CoursesList/Courses";
 import { Login } from "./pages/Login";
-import { MAIN_ROUTE, LOGIN_ROUTE } from "./utils/consts";
+import { MAIN_ROUTE, LOGIN_ROUTE, COURSE_ROUTE } from "./utils/consts";
 
 export const authRoutes = [
-    {
-      path: MAIN_ROUTE,
-      Component: Courses
-    },
-]
+  {
+    path: MAIN_ROUTE,
+    Component: Courses,
+  },
+  {
+    path: COURSE_ROUTE,
+    Component: CoursePage,
+  },
+];
 
 export const publicRoutes = [
-    {
-      path: LOGIN_ROUTE,
-      Component: Login  
-    },
-]
+  {
+    path: LOGIN_ROUTE,
+    Component: Login,
+  },
+];
