dee8addf9d1dd497864149039540fbf183274883
loyal_goose <polina.alina.2015@mail.ru>
2023-05-16 16:49:43 +0700

    feat: добавить компонентам курсов свойство wrap

--- src/pages/CoursesList/Courses.tsx
+++ src/pages/CoursesList/Courses.tsx
@@ -17,13 +17,13 @@ export const Courses = () => {
   }, []);
 
   return (
-    <div className=" w-5/6">
+    <div className=" 2xl:w-11/12 xl:w-11/12 lg:w-5/6  ">
       <label className="block text-gray-700 text-3xl font-bold mb-2">
         Мои курсы
       </label>
       <div
         role="list"
-        className="flex justify-between  relative divide-y p-10 "
+        className="flex flex-wrap justify-between  relative divide-y p-10 "
       >
         {courses.map((course) => (
           <CourseItem {...course} />
--- src/pages/CoursesList/components/CourseItem.tsx
+++ src/pages/CoursesList/components/CourseItem.tsx
@@ -5,7 +5,7 @@ export const CourseItem = (course: Course) => {
   let navigate = useNavigate();
 
   return (
-    <div className="justify-around flex flex-col flex-wrap gap-x-6 mx-5 p-5 w-96 h-48 divide-gray-100 rounded-lg bg-gray-100">
+    <div className="justify-around flex flex-col flex-wrap gap-x-6 my-4 mx-3 p-5 w-96 xl:w-92 h-48 divide-gray-100 rounded-lg bg-gray-100">
       <p className="text-lg font-semibold leading-6 text-gray-900 ">
         {course.fullname}
       </p>
@@ -13,7 +13,7 @@ export const CourseItem = (course: Course) => {
         Преподаватель: {course.id}
       </p>
       <div
-        className=" w-28 px-2 py-1.5 text-center text-white bg-blue-500 hover:bg-blue-700 hover:cursor-pointer rounded-full"
+        className="w-28 px-2 py-1.5 text-center text-white bg-blue-500 hover:bg-blue-700 hover:cursor-pointer rounded-full"
         onClick={() => navigate(`/course/${course.id}`)}
       >
         Перейти &rarr;
