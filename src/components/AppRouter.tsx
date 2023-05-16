55ec54bfd4eee2dbfdb1f1bee106974a55d95095
loyal_goose <polina.alina.2015@mail.ru>
2023-05-16 16:22:03 +0700

    feat: получить информацию курса

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,15 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
-import { authRoutes, publicRoutes } from '../routes';
+import { Routes, Route } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
 
 export const AppRouter = () => {
-  const authRouteComponents = authRoutes.map(({ path, Component }) => <Route path={path} element={<Component />} key={path} />);
-  const publicRouteComponents = publicRoutes.map(({ path, Component }) => <Route path={path} element={<Component />} key={path} />);
+  const authRouteComponents = authRoutes.map(({ path, Component }) => (
+    <Route path={path} element={<Component />} key={path} />
+  ));
+  const publicRouteComponents = publicRoutes.map(({ path, Component }) => (
+    <Route path={path} element={<Component />} key={path} />
+  ));
 
   return (
-
     <Routes>
       {authRouteComponents}
       {publicRouteComponents}
     </Routes>
-  )
-}
+  );
+};
--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -1,5 +1,41 @@
-export const CoursePage = () => {
-  //dev.online.tusur.ru/moodle/webservice/rest/server.php?wstoken=2b8e54a638f0422b6859f223fa0a086e&wsfunction=core_course_get_contents&moodlewsrestformat=json&courseid=2
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 
-  https: return <>course</>;
+export interface info {
+  id: number;
+  name: string;
+  modules: [];
+}
+
+export const CoursePage = () => {
+  const [courseInfo, setCourseInfo] = useState<info[]>([]);
+  const token = "2b8e54a638f0422b6859f223fa0a086e";
+  const func = "core_course_get_contents";
+  const params = useParams();
+
+  useEffect(() => {
+    const apiUrl = `https://dev.online.tusur.ru/moodle/webservice/rest/server.php?wstoken=${token}&wsfunction=${func}&moodlewsrestformat=json&courseid=${params.id}`;
+    setTimeout(() => {
+      axios.get(apiUrl).then((resp) => {
+        const info = resp.data;
+        setCourseInfo(info);
+      });
+    });
+  }, []);
+
+  return (
+    <div className="flex flex-col content-center">
+      {courseInfo.map((info) => (
+        <>
+          <div>{info.name}</div>
+          <div className="flex flex-col m-5">
+            {info.modules.map((module: any) => (
+              <div>{module.name}</div>
+            ))}
+          </div>
+        </>
+      ))}
+    </div>
+  );
 };
