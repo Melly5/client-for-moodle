46dc41fb8e5150533864903dd8e3f1de5a61c158
loyal_goose <polina.alina.2015@mail.ru>
2023-05-17 14:54:47 +0700

    feat: поправить передаче пропсов на страницу папки

--- src/components/Folder/Folder.tsx
+++ src/components/Folder/Folder.tsx
@@ -1,25 +1,20 @@
 import { FolderOpenIcon } from "@heroicons/react/24/outline";
 import { useNavigate } from "react-router-dom";
 
-export const Folder = (module: any) => {
+export const Folder = (folder: any) => {
   let navigate = useNavigate();
 
   return (
     <div className=" flex">
       <FolderOpenIcon className="h-6 w-6 mr-3" aria-hidden="true" />
-      <div onClick={() => navigate(`/folder/${module.id}`)}>{module.name}</div>
-      <div>
-        {module.contents.map((content: any) => (
-          <>
-            <a
-              href={`${content.fileurl}&token=2b8e54a638f0422b6859f223fa0a086e`}
-              download
-              className="mx-3"
-            >
-              {content.filename}
-            </a>
-          </>
-        ))}
+      <div
+        onClick={() =>
+          navigate(`/folder/${folder.id}`, {
+            state: { name: folder.name, content: folder.contents },
+          })
+        }
+      >
+        {folder.name}
       </div>
     </div>
   );
--- src/components/Folder/FolderPage.tsx
+++ src/components/Folder/FolderPage.tsx
@@ -1,9 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
-type Props = {};
+export const FolderPage = () => {
+  const { state } = useLocation();
+  const { name, content } = state;
 
-export const FolderPage = (props: Props) => {
-  const params = useParams();
-  console.log(params);
-  return <div>FolderItem</div>;
+  console.log(state);
+
+  return (
+    <div>
+      {name}
+      <div>
+        {content.map((content: any) => (
+          <>
+            <a
+              href={`${content.fileurl}&token=2b8e54a638f0422b6859f223fa0a086e`}
+              download
+              className="mx-3"
+            >
+              {content.filename}
+            </a>
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
