66cdaa19d6d0e5bb11a21fd0430e17c110702eaa
loyal_goose <polina.alina.2015@mail.ru>
2023-05-17 15:25:05 +0700

    feat: добавить парсер строки от сервера для форума

--- src/components/Forum/Forum.tsx
+++ src/components/Forum/Forum.tsx
@@ -1,14 +1,21 @@
 import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/outline";
 import { useNavigate } from "react-router-dom";
 
-export const Forum = (module: any) => {
+export const Forum = (forum: any) => {
   let navigate = useNavigate();
 
   return (
     <div className=" flex">
       <ChatBubbleLeftRightIcon className="h-6 w-6 mr-3" aria-hidden="true" />
-      <div onClick={() => navigate(`/forum/${module.id}`)}>{module.name}</div>
+      <div
+        onClick={() =>
+          navigate(`/forum/${forum.id}`, {
+            state: { id: forum.instance, name: forum.name },
+          })
+        }
+      >
+        {forum.name}
+      </div>
     </div>
   );
 };
-//{"error":"A required parameter (token) was missing","errorcode":"missingparam","stacktrace":null,"debuginfo":null,"reproductionlink":null}
--- src/components/Forum/ForumPage.tsx
+++ src/components/Forum/ForumPage.tsx
@@ -1,9 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import parse from "html-react-parser";
 
-type Props = {};
+export const ForumPage = () => {
+  const [discussions, setDiscussions] = useState<any[]>([]);
+  const { state } = useLocation();
+  const { id, name } = state;
 
-export const ForumPage = (props: Props) => {
-  const params = useParams();
-  console.log(params);
-  return <div>ForumItem</div>;
+  useEffect(() => {
+    const apiUrl = `https://dev.online.tusur.ru/moodle/webservice/rest/server.php?wstoken=2b8e54a638f0422b6859f223fa0a086e&wsfunction=mod_forum_get_forum_discussions&moodlewsrestformat=json&forumid=${id}`;
+    setTimeout(() => {
+      axios.get(apiUrl).then((resp) => {
+        const data = resp.data;
+        setDiscussions(data.discussions);
+      });
+    });
+  }, []);
+
+  console.log(discussions);
+  return (
+    <div>
+      <div>{name}</div>
+      <div>
+        {discussions.map((discussion: any) => (
+          <div>
+            <div>{discussion.name}</div>
+            <div>{parse(discussion.message)}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
