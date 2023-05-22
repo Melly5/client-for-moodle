3e4ed8b59ab9b73b92948374feded7b70101a71d
loyal_goose <polina.alina.2015@mail.ru>
2023-05-22 11:35:15 +0700

    feat: обновить пути к страницам

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
-import { AssignPage } from "./components/Assign/AssignPage";
-import { FolderPage } from "./components/Folder/FolderPage";
-import { ForumPage } from "./components/Forum/ForumPage";
-import { LessonPage } from "./components/Lesson/LessonPage";
-import { QuizPage } from "./components/Quiz/QuizPage";
-import { ResourcePage } from "./components/Resource/ResourcePage";
+import { AssignPage } from "./pages/AssignPage/AssignPage";
+import { FolderPage } from "./pages/FolderPage/FolderPage";
+import { ForumPage } from "./pages/ForumPage/ForumPage";
+import { LessonPage } from "./pages/LessonPage/LessonPage";
+import { QuizPage } from "./pages/QuizPage/QuizPage";
+import { ResourcePage } from "./pages/ResourcePage/ResourcePage";
 import { CoursePage } from "./pages/CoursePage/CoursePage";
 import { Courses } from "./pages/CoursesList/Courses";
 import { Login } from "./pages/Login";
